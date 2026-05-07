3de6e87ff77e37ce5677779c928c8bcccda8467c
Viveloja <estuarlito@gmail.com>
2026-05-07 01:09:38 -0500

    error 6

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'adminaj.totemmassmedia.com',
+      },
+      {
+        protocol: 'https',
         hostname: '*.aprendiendojuntos.ec',
       },
     ],
