1116a203238f8107f03c7c8663111656cf17976e
Viveloja <estuarlito@gmail.com>
2026-06-04 01:38:59 -0500

    bar admin fix hmac

--- app/api/admin-bar/auth/route.ts
+++ app/api/admin-bar/auth/route.ts
@@ -16,7 +16,7 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({ authenticated: false })
   }
 
-  return verifyToken(match[1])
+  return verifyToken(decodeURIComponent(match[1]))
 }
 
 export async function POST(request: NextRequest) {
